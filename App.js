560383e399b418ca35a59257def5e44c5e0a3bbc
ileneslimen <slimennorhen1995@gmail.com>
2021-06-17 13:20:26 +0100

    intro-react

---
+++ App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import About from './Components/About';
+import Navbar from './Components/Navbar'
+import Carous from './Components/Carousel'
+
+function App() {
+  return (
+    <div className="App">
+   <Navbar/>
+  <About/>
+  <Carous/>
+    </div>
+  );
+}
+
+export default App;
